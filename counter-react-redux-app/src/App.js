11b624ee8d887c840327ec918f525fec4eb64298
Md <mdakram12022002@gmail.com>
2024-10-26 19:31:57 +0530

    counder-react-redux-app is builded

---
+++ counter-react-redux-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen mx-auto">
+      <Counter />
+    </div>
+  );
+}
+export default App;
