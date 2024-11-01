8a2d6705576192aa05ec458b32958d6cc606220b
Md <mdakram12022002@gmail.com>
2024-11-01 20:36:25 +0530

    my portfolio

---
+++ my-portfolio/src/components/Projects.jsx
@@ -0,0 +1,116 @@
+/** @format */
+import ApplicationForm from "../assets/applicationForm.png";
+import OurTestimonials from "../assets/ourTestimonials.png";
+import PlanForAdven from "../assets/planForAdventure.png";
+import StudyPoint from "../assets/studyPoint.png";
+import TopTenCourse from "../assets/topTenCourses.png";
+import EasyShopping from "../assets/easyShopping.png";
+import Amazon from "../assets/amazon.png";
+
+const projects = [
+  {
+    id: 1,
+    name: "Easy Shopping",
+    technologies: "JavaScript, React-Redux, and Tailwind",
+    image: EasyShopping,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/online_shopping",
+    netlify: "https://easyshoppingin.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Study Point",
+    technologies: "JavaScript, React, and Tailwind CSS",
+    image: StudyPoint,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/study-point",
+    netlify: "https://studypointakram.netlify.app/login",
+  },
+  {
+    id: 3,
+    name: "Our Testimonial",
+    technologies: "JavaScript, React, and Vanilla CSS",
+    image: OurTestimonials,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/our-testimonials",
+    netlify: "https://our-review-akram.netlify.app",
+  },
+  {
+    id: 4,
+    name: "Form Aplication",
+    technologies: "JavaScript, React, and Vanilla CSS",
+    image: ApplicationForm,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/formapplication",
+    netlify: "https://applicationformin.netlify.app",
+  },
+  {
+    id: 5,
+    name: "Plan For Advanture",
+    technologies: "JavaScript, React, and Tailwind CSS",
+    image: PlanForAdven,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/plan-for-advanture",
+    netlify: "https://plan-for-adventure.netlify.app",
+  },
+  {
+    id: 6,
+    name: "Top Ten Courses",
+    technologies: "JavaScript, React, and Tailwindcss",
+    image: TopTenCourse,
+    github:
+      "https://github.com/mdakram2002/React.js-WebDev/tree/main/top-ten-courses",
+    netlify: "https://toptencourses.netlify.app",
+  },
+  {
+    id: 7,
+    name: "Amazon Clone",
+    technologies: "HTML & CSS",
+    image: Amazon,
+    github: "https://github.com/mdakram2002/Amazon-Clone",
+    netlify: "https://amazon-clone-akram.netlify.app",
+  },
+];
+const Projects = () => {
+  return (
+    <div className="bg-black text-white py-20" id="project">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className="bg-gray-900 p-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105"
+            >
+              <img
+                className="rounded-lg mb-4 object-cover"
+                src={project.image}
+                alt={project.name}
+              />
+              <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+              <p className="text-gray-400 mb-4">{project.technologies}</p>
+              <a
+                className="inline-block bg-gradient-to-r from-green-500 to-blue-700 text-white px-4 py-2 rounded-full mr-2"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={project.github}
+              >
+                GitHub
+              </a>
+              <a
+                className="inline-block bg-gradient-to-r from-green-500 to-blue-700 text-white px-4 py-2 rounded-full"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={project.netlify}
+              >
+                Netlify
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
